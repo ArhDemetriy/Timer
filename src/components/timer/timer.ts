24dfdaade2e9b8af4bca967a77eb42b20de834c3
ArhDemetriy <ArhDemetriy@GMail.com>
2020-09-29 16:08:47 +0800

    in timer: add trow null ref readonly for const add events for control button add tick handler add destructor

--- src/components/timer/timer.ts
+++ src/components/timer/timer.ts
@@ -4,55 +4,100 @@ interface iDestructible {
 type arrayDestructible = [() => void, ...Array<iDestructible>];
 
 class Timer implements iDestructible{
-  protected SEC = 1000;
-  protected MIN = 60 * this.SEC;
-  protected HOU = 60 * this.MIN;
+  protected readonly SEC = 1000;
+  protected readonly MIN = 60 * this.SEC;
+  protected readonly HOU = 60 * this.MIN;
 
-  protected countTime: Date;
+  protected countedTime: Date;
   protected lastStartTime : Date;
   protected isActive : boolean;
   protected idInterval: number | null;
-  protected selfElement: Element;
+  protected readonly selfElement: Element;
+
+  private readonly buttonPlayPauseToggle: Element;
+  private readonly buttonStop: Element;
   constructor(element: Element) {
+    if (!element.querySelector('.timer__input')) throw new ReferenceError(
+      `not elem ".timer__input" in tag: ${element.tagName} class: ${element.classList.toString}`);
+
+    let temp = element.querySelector('.timer__button-play_pause');
+    if (!temp) throw new ReferenceError(
+      `not elem ".timer__button-play_pause" in tag: ${element.tagName} class: ${element.classList.toString}`);
+    this.buttonPlayPauseToggle = temp;
+
+    temp = element.querySelector('.timer__button-stop');
+    if (!temp) throw new ReferenceError(
+      `not elem ".timer__button-stop" in tag: ${element.tagName} class: ${element.classList.toString}`);
+    this.buttonStop = temp;
     this.selfElement = element;
-    this.countTime = new Date(0);
+    this.countedTime = new Date(0);
     this.lastStartTime = new Date();
     this.isActive = false;
     this.idInterval = null;
+
+    this.buttonPlayPauseToggle.addEventListener('click', this.playPauseToggle);
+    this.buttonStop.addEventListener('click', this.stop);
   }
-  play() {
-    if (!this.idInterval)
-      clearInterval(this.idInterval);
-    this.lastStartTime.setTime(this.countTime.getTime() + Date.now());
+  protected play() {
+    if (!isNaN(this.idInterval!))
+      clearInterval(this.idInterval!);
+    this.lastStartTime.setTime(this.countedTime.getTime() + Date.now());
     this.isActive = true;
     this.idInterval = setInterval(this.tick.bind(this), 1000);
   }
-  pause() {
+  protected pause() {
     this.isActive = false;
-    this.countTime.setTime(Date.now() - this.lastStartTime.getTime() + this.countTime.getTime());
-    clearInterval(this.idInterval);
+    this.countedTime.setTime(Date.now() - this.lastStartTime.getTime() + this.countedTime.getTime());
+    if (!isNaN(this.idInterval!))
+      clearInterval(this.idInterval!);
   }
   private tick() {
-    if (!this.isActive) return;
+    let lengthTime = Date.now();
 
-    let lengthTime = Date.now() - this.lastStartTime.getTime();
+    if (!this.isActive) return;
+    const element = this.selfElement.querySelector('.timer__input');
+    if (!element) return;
+
+    lengthTime -= this.lastStartTime.getTime();
 
     let temp = Math.trunc((lengthTime % this.MIN) / this.SEC);
-    // let lengthTimeStr = temp.toString().padStart(2, '0');
-    // if (lengthTime / this.MIN >= 1) {
-    //   lengthTime -= temp;
-    //   temp = Math.trunc((lengthTime % this.HOU) / this.MIN);
-    //   lengthTimeStr = temp.toString().padStart(2, '0') + ':' + lengthTimeStr;
-    //   if (lengthTime / this.HOU >= 1) {
-    //     lengthTime -= temp;
-    //     temp = Math.trunc(lengthTime / this.HOU);
-    //     lengthTimeStr = temp.toString().padStart(2, '0') + ':' + lengthTimeStr;
-    //   }
-    // }
-    // element.querySelector('.timer__input').value = lengthTimeStr;
+    let lengthTimeStr = temp.toString().padStart(2, '0');
+    if (lengthTime / this.MIN >= 1) {
+      lengthTime -= temp;
+      temp = Math.trunc((lengthTime % this.HOU) / this.MIN);
+      lengthTimeStr = temp.toString().padStart(2, '0') + ':' + lengthTimeStr;
+      if (lengthTime / this.HOU >= 1) {
+        lengthTime -= temp;
+        temp = Math.trunc(lengthTime / this.HOU);
+        lengthTimeStr = temp.toString().padStart(2, '0') + ':' + lengthTimeStr;
+      }
+    }
 
+    if (element.hasOwnProperty('value'))
+      (element as HTMLInputElement).value = lengthTimeStr
+    else
+      element.textContent = lengthTimeStr;
+  }
+  playPauseToggle() {
+    if (this.isActive)
+      this.pause()
+    else
+      this.play();
+  }
+  stop() {
+    this.isActive = false;
+    if (!isNaN(this.idInterval!))
+      clearInterval(this.idInterval!);
+    this.countedTime.setTime(0);
   }
   destruct() {
-    this.selfElement.removeEventListener('click', clickOnButton);
+    this.stop();
+    this.buttonPlayPauseToggle.removeEventListener('click', this.playPauseToggle);
+    this.buttonStop.removeEventListener('click', this.stop);
   }
 }
+
+const elements = document.querySelectorAll('.timer');
+elements.forEach((_, i, a) => {
+  (a[i] as any).myTimer = new Timer(a[i]);
+});

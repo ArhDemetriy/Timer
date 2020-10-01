e17e16f6bc75da0bdbefb8ed7c7904539fd81331
ArhDemetriy <ArhDemetriy@GMail.com>
2020-10-01 12:49:07 +0800

    gh-pages test

---
+++ adder.8d707ee4.js
@@ -0,0 +1,3 @@
+// ASSET: C:\Development\Projects\Timer\src\components\adder\adder.ts
+document.querySelectorAll('.adder').forEach(q => q.addEventListener('click', () => console.log('test messeg add new timer')));
+//# sourceMappingURL=adder.8d707ee4.js.map
--- adder.e5a74d3d.js
+++
@@ -1,2 +0,0 @@
-document.querySelectorAll(".adder").forEach(e=>e.addEventListener("click",()=>console.log("test messeg add new timer")));
-//# sourceMappingURL=adder.e5a74d3d.js.map

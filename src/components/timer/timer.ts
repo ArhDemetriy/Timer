2125f69871a7ec6280c8909e477010d8dc9f9d41
ArhDemetriy <ArhDemetriy@GMail.com>
2020-10-04 10:56:55 +0800

    init template

--- src/components/timer/timer.ts
+++ src/components/timer/timer.ts
@@ -2,11 +2,13 @@ import Timer from './timer__class';
 import * as timerInterfaces from './timer__class';
 
 const arrayOfDestructible: timerInterfaces.ArrayOfDestructible = [
-  function () {
+  function qwe(this:any) {
     for (let i = 1; i < this.length; this[i++].destruct()) { }
   },
 ];
 
-(document as any).q = arrayOfDestructible;
+(document as any).arrayOfTimer = arrayOfDestructible;
+(document as any).Timer = Timer;
 
-document.querySelectorAll('.timer').forEach(q => arrayOfDestructible.push((q as any).myTimer = new Timer(q)));
+
+// document.querySelectorAll('.timer').forEach(q => arrayOfDestructible.push((q as any).myTimer = new Timer(q)));
